4aa8eb00a587cb73d862aafc544692ffe1c166a8
Klym <klim.oleksienko@gmail.com>
2022-09-04 13:12:49 +0300

    done upper part and add photo from storage

---
+++ Third/second.js
@@ -0,0 +1,27 @@
+let beers = []
+
+        function requestData() {
+            fetch('https://api.punkapi.com/v2/beers/random').then(data => data.json())
+            
+                .then(data => beers = data)
+                .then(() => renderBeers())
+        }
+        function renderBeers() {
+            const container = document.getElementById('container');
+            console.log(beers)
+                const nameOfBeer = document.getElementById('name');
+                nameOfBeer.innerText = beers[0].name;
+                const picOfBeer = document.getElementById('pictureLeft');
+                const smallPicOfBeer= document.getElementById("smallPictureLeftDown")
+                
+                const img = document.createElement('img');
+                img.classList.add('img')
+                img.src = beers[0].image_url;
+                picOfBeer.appendChild(img);
+                const img2 = document.createElement('img');
+                img2.classList.add('img2');
+                img2.src = beers[0].image_url;
+                smallPicOfBeer.appendChild(img2)
+        }
+
+        requestData();
